1906e0074f55795dce80522b7eaff77c00f8bb6d
Pacifist5543 <hoholgg433@gmail.com>
2025-04-21 11:53:38 +0700

    Update game.py

--- farmer bot/handlers/game.py
+++ farmer bot/handlers/game.py
@@ -18,35 +18,112 @@ class CallbackHandler:
         Args:
             bot (TeleBot): объект бота
         """
-        # TODO: описать функцию
-        ...
+        self.bot = bot
+
+        self.bot.register_callback_query_handler(
+            self.handle_main_menu,  # запускаем эту функцию
+            func=lambda call: call.data
+            == kb.START_GAME,  # если получили вот такой сигнал
+        )
+        self.bot.register_callback_query_handler(
+            self.handle_update_info,  # запускаем эту функцию
+            func=lambda call: call.data
+            == kb.UPDATE_INFO,  # если получили вот такой сигнал
+        )
+        self.bot.register_callback_query_handler(
+            self.handle_upgrade_level,  # запускаем эту функцию
+            func=lambda call: call.data
+            == kb.UPGRADE_LEVEL,  # если получили вот такой сигнал
+        )
+        self.bot.register_callback_query_handler(
+            self.handle_friends_list,  # запускаем эту функцию
+            func=lambda call: call.data
+            == kb.FRIENDS_LIST,  # если получили вот такой сигнал
+        )
+        self.bot.register_callback_query_handler(
+            self.handle_back_to_main,  # запускаем эту функцию
+            func=lambda call: call.data
+            == kb.BACK_TO_MAIN,  # если получили вот такой сигнал
+        )
 
     def handle_main_menu(self, call: types.CallbackQuery):
         """Обработка главного игрового меню."""
 
-        # TODO: описать функцию
-        ...
+        user = GameService().get_user(call.from_user)
+
+        invited_players = GameService().get_friends_list(call.from_user)
+        passive_income = sum([player.level for player in invited_players]) / 4
+
+        self.bot.send_message(
+            call.message.chat.id,
+            text=self.MAIN_TEXT.format(
+                coins=user.balance,
+                level=user.level,
+                invited=passive_income,
+                price=user.level**2,
+            ),
+            reply_markup=kb.game_kb_main,
+        )
+        self.bot.answer_callback_query(call.id)
 
     def handle_update_info(self, call: types.CallbackQuery):
         """Обработка кнопки "обновить", которая редактирует главное меню, актуализируя данные."""
 
-        # TODO: описать функцию
-        ...
+        user = GameService().get_user(call.from_user)
+
+        invited_players = GameService().get_friends_list(call.from_user)
+        passive_income = sum([player.level for player in invited_players]) / 4
+
+        try:
+            self.bot.edit_message_text(
+                chat_id=call.message.chat.id,
+                message_id=call.message.id,
+                text=self.MAIN_TEXT.format(
+                    coins=user.balance,
+                    level=user.level,
+                    invited=passive_income,
+                    price=user.level**2,
+                ),
+                reply_markup=kb.game_kb_main,
+            )
+        except apihelper.ApiTelegramException as e:
+            pass
+        self.bot.answer_callback_query(call.id)
 
     def handle_friends_list(self, call: types.CallbackQuery):
         """Отображение списка друзей и пригласительного года при нажатии на кнопку "список друзей"."""
 
-        # TODO: описать функцию
-        ...
+        invited_players = GameService().get_friends_list(call.from_user)
+
+        text = "У тебя нет друзей, выйди на улицу хотябы"
+
+        if invited_players:
+            text = "Ваши друзья:\n"
+            for player in invited_players:
+                text += f"@{player.telegram_name} - LVL {player.level}\n"
+
+        text += f"\n\n[Ваш пригласительный код: {call.from_user.id}]"
+
+        self.bot.edit_message_text(
+            text,
+            call.message.chat.id,
+            call.message.id,
+            reply_markup=kb.game_kb_back_to_main,
+        )
+        self.bot.answer_callback_query(call.id)
 
     def handle_back_to_main(self, call: types.CallbackQuery):
         """Возвращает пользователя в главное меню при нажатии кнопки "назад" в списке друзей."""
-
-        # TODO: описать функцию
-        ...
+        self.handle_update_info(call)
 
     def handle_upgrade_level(self, call: types.CallbackQuery):
         """Обработка улучшения уровня на кпноку "повысить уровень"."""
 
-        # TODO: описать функцию
-        ...
+        if not GameService().upgrade_level(call.from_user):
+            self.bot.answer_callback_query(
+                call.id, "Недостаточно коинов для прокачки уровня.", show_alert=True
+            )
+            return
+
+        self.handle_update_info(call)
+
